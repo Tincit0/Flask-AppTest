aa21fcf5c387b86b8f53ce043e7767df70328780
Tin <77906615+Tincit0@users.noreply.github.com>
2022-07-06 03:12:10 -0300

    Bloque para subir fotos y guardarlas. OP fallida. Tengo sueño.
    Agregué algunos comentarios y un bloque de codigo para subir una foto y guardarla. Tira un error de file not found porque seguro debe haber un problema en  "_foto.save("uploads/"+nuevoNombreFoto)". Pero ya es tarde y no quiero seguir. Suer3

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import render_template
 from flaskext.mysql import MySQL
 from flask import render_template,request
-
+from datetime import datetime
 
 app = Flask(__name__)
 mysql = MySQL()
@@ -30,12 +30,22 @@ def create():
 @app.route('/store', methods = ['POST'])
 def storage():
     
+    #pedimos el form de create.html
     _nombre = request.form['txtNombre']
     _correo = request.form['txtCorreo']
     _foto = request.files['txtFoto']
     
+    #bloque para guardar y nombrar la foto q suban
+    nuevoNombreFoto=''
+    now = datetime.now()
+    tiempo = now.strftime("%Y%H%M%S")
+    if _foto.filename!='':
+        nuevoNombreFoto = tiempo+_foto.filename
+        _foto.save("uploads/"+nuevoNombreFoto)
+    
+    #definimos y lanzamos la query
     sql = "INSERT INTO `sistema`.`empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, %s, %s, %s);"
-    datos=(_nombre,_correo,_foto.filename)
+    datos=(_nombre,_correo,nuevoNombreFoto)
     conn = mysql.connect()
     cursor = conn.cursor()
     cursor.execute(sql,datos)
